004dbf57b337f852663e8a4160eb0006f8d02972
nelli-belovol <nelli199020@gmail.com>
2024-05-31 16:24:43 +0300

    TaskItem styles

--- app/Components/TaskItem/TaskItem.tsx
+++ app/Components/TaskItem/TaskItem.tsx
@@ -2,6 +2,7 @@
 
 import { Task, useGlobalState } from '@/app/context/globalProvider';
 import { edit, trash } from '@/app/utils/Icons';
+import formatDate from '@/app/utils/formatDate';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -16,7 +17,7 @@ const TaskItem = ({ task }: Props) => {
     <TaskItemStyled className="task" key={id} theme={theme}>
       <h2>{title}</h2>
       <p>{description}</p>
-      <p className="date">{date}</p>
+      <p className="date">{formatDate(date)}</p>
       <div className="task-footer">
         {isCompleted ? (
           <button className="completed">Completed</button>
@@ -43,5 +44,44 @@ const TaskItemStyled = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
+
+  .date {
+    margin-top: auto;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  .task-footer {
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+
+    button {
+      border: none;
+      outline: none;
+      cursor: pointer;
+      i {
+        font-size: 1.4rem;
+        color: ${props => props.theme.colorGrey2};
+      }
+    }
+    .edit {
+      margin-left: auto;
+    }
+    .completed,
+    .incomplete {
+      display: inline-block;
+      padding: 0.4rem 1rem;
+      background: ${props => props.theme.colorDanger};
+      border-radius: 30px;
+    }
+
+    .completed {
+      background: ${props => props.theme.colorGreenDark};
+    }
+  }
 `;
 export default TaskItem;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import './globals.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import GlobalStylesProvider from './providers/GlobalStylesProvider';
@@ -7,7 +7,10 @@ import ContextProvider from './providers/ContextProvider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -33,7 +36,7 @@ export default function RootLayout({
             referrerPolicy="no-referrer"
           />
         </head>
-        <body className={inter.className}>
+        <body className={nunito.className}>
           <ContextProvider>
             <GlobalStylesProvider>
               {userId && <Sidebar />}
